aa080ad510235b52e623b9aec8fea42f04057c63
Jimmy DC <jimmydalecleveland@gmail.com>
2021-04-04 11:47:44 -0600

    add test for getRandomIntInRange

--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -1,9 +1,17 @@
-const { getRandomInt } = require("./helper");
+const { getRandomInt, getRandomIntInRange } = require("./helper");
 
 test("gets a random integer in a given range", () => {
   for (let i = 0; i < 20; i++) {
     const result = getRandomInt(3);
     expect(result).toBeGreaterThanOrEqual(0);
     expect(result).toBeLessThan(3);
+  }
+});
+
+test("gets a random integer in a given range", () => {
+  for (let i = 0; i < 20; i++) {
+    const result = getRandomIntInRange(4, 9);
+    expect(result).toBeGreaterThanOrEqual(4);
+    expect(result).toBeLessThan(9);
   }
 });
